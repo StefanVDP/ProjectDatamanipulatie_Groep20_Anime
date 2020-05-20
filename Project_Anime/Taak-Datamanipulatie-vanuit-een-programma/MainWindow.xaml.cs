f4622863d700bef8b8f441707dde8c9f18ad2170
StefanVDP <61277081+StefanVDP@users.noreply.github.com>
2020-05-20 09:37:18 +0200

    test

---
+++ Project_Anime/Taak-Datamanipulatie-vanuit-een-programma/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectDatamanipulatieAnime_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Application.Current.Shutdown();
+
+        }
+
+        private void BtnSeizoen_Click(object sender, RoutedEventArgs e)
+        {
+            Window Seizoen = new SerieWindow();
+            Seizoen.Show();
+            this.Close();
+        }
+
+        private void BtnManga_Click(object sender, RoutedEventArgs e)
+        {
+            Window Manga = new MangaWindow();
+            Manga.Show();
+            this.Close();
+        }
+
+        private void BtnIndustrie_Click(object sender, RoutedEventArgs e)
+        {
+            //Window Industrie = new IndustrieWindow();
+            // Industrie.Show();
+            //this.Close();
+        }
+
+        private void BtnGenre_Click(object sender, RoutedEventArgs e)
+        {
+            //Window Genre = new GenreWindow();
+            //Genre.Show();
+            //this.Close();
+        }
+
+        private void BtnManage_Click(object sender, RoutedEventArgs e)
+        {
+            //Window Manage = new ManageWindow();
+            //Manage.Show();
+            //this.Close();
+        }
+    }
+}
